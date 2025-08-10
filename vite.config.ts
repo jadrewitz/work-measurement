8ead89b886297a5a6703fc28605d6f7051852fc5
Jason <jason@drewitz.me>
2025-08-10 19:59:37 -0400

    Fix asset paths: set Vite base to './'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',          // <= important: use relative asset paths
+  build: { outDir: 'dist' }
 })
